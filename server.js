7e00333332508db7dc0ef0f48d719d2d6ad35003
sandip saw <sandipsaw449@gmail.com>
2025-10-03 09:07:41 +0530

    frontend

--- server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const connectToDB = require('./src/db/db')
-const app = require('./src/app')
-
-connectToDB();
-
-app.listen(3000,()=>{
-    console.log("server is running on port http://localhost:3000");
-})
--- src/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const {registerController} = require('../controller/auth.controller');
-const {loginController} = require('../controller/auth.controller')
-const router = express.Router();
-
-router.post('/register',registerController);
-router.post('/login',loginController);
-
-module.exports = router
